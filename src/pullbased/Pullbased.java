6adcc5339d8405da0f21e450c4f78840ac413e01
Maria Theresa R <maria.theresa@ti.ukdw.ac.id>
2018-05-11 09:34:51 +0700

    Fungsi 1 getWarnaSisi

--- src/pullbased/Pullbased.java
+++ src/pullbased/Pullbased.java
@@ -15,7 +15,7 @@ public class Pullbased {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Antarmuka().setVisible(true);
     }
     
 }
