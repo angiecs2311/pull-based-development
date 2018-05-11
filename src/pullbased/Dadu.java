c95cb55a254ef8fabb1d016ded2ec65b71abac05
Unknown <luh.muryanti@ti.ukdw.ac.id>
2018-05-11 14:10:35 +0700

    Fungsi 2. Perbaikan Fungsi Lempar

--- src/pullbased/Dadu.java
+++ src/pullbased/Dadu.java
@@ -12,29 +12,39 @@ import java.util.Random;
  */
 
 public final class Dadu {
-public static final int DEFAULT_SISI = 6;
-private static Random randGenerator = new Random();
-private final int jumSisi;
-private int hasil;
-public Dadu() {
-this(DEFAULT_SISI);
-}
-public Dadu(int numSides) {
-this.jumSisi = numSides;
-}
-public int lempar() {
-hasil = randGenerator.nextInt(jumSisi) + 1;
-return hasil;
-}
-public int getJumSisi() {
-return jumSisi;
-}
-public int getHasil() {
-return hasil;
-}
-@Override
-public String toString() {
-return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil();
-}
+    public static final int DEFAULT_SISI = 6;
+    private static Random randGenerator = new Random();
+    private final int jumSisi;
+    private int hasil, tamp = 0;
+    public Dadu() {
+        this(DEFAULT_SISI);
+    }
+    
+    public Dadu(int numSides) {
+        this.jumSisi = numSides;
+    }
+    
+    public int lempar() {
+        do{
+            hasil = randGenerator.nextInt(jumSisi) + 1;
+        } while(hasil == tamp);
+        System.out.println("Hasil: "+hasil);
+        tamp = hasil;
+        
+        return hasil;
+    }
+    
+    public int getJumSisi() {
+        return jumSisi;
+    }
+    
+    public int getHasil() {
+        return hasil;
+    }
+    
+    @Override
+    public String toString() {
+        return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil();
+    }
 }
 
